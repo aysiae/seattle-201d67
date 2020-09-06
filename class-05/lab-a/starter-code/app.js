ea4b104b6cd5e9f1f6930f49a00c5571cd4c8d06
Aysia Brown <aysiaebrown@gmail.com>
2020-09-06 13:53:44 -0700

    added multiply anny array

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -110,7 +110,7 @@ function multiplyArray(multArr) { //eslint-disable-line
         total = multiply(total,multArr[i])[0];
     }
     var ret = [total, "The numbers " + multArr + " have a product of " + total + "."];
-    return ret;
+    return ret
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
@@ -138,10 +138,15 @@ Test this function by hand in the console to get it working, and when you think 
 var testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
-
+    var total = 1;
+    for(var i = 0; i < dynamicArray.length; i++) {
+        total = multiply(total,dynamicArray[i])[0];
+    }
+    var ret = [total, "The numbers " + dynamicArray + " have a product of " + total + "."];
+    return ret
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
