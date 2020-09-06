68ca67ec193b788a936ed049cbe05c502de59dd9
Aysia Brown <aysiaebrown@gmail.com>
2020-09-06 13:34:15 -0700

    sum and multiple function added

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -52,11 +52,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+    var sumAB = sum(a,b)[0];
+    var sumTotal = sum(sumAB,c)[0];
+    var multAB = multiply(a,b)[0];
+    var multTotal = multiply(multAB,c)[0];
+    var ret = [sumTotal,multTotal, a + " and " + b + " and " + c + " sum to " + sumTotal + ".","The product of " + a + " and " + b + " and " + c + " is " + multTotal + "."]
+    return ret;     
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+ testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
