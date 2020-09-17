8713518e13e1cfcf3df56806ea3c3d79b17f4bdc
Aysia Brown <aysiaebrown@gmail.com>
2020-09-17 16:45:07 -0700

    started filling in functions for lab14

--- class-14/lab/starter-code/js/catalog.js
+++ class-14/lab/starter-code/js/catalog.js
@@ -9,12 +9,14 @@ var cart = new Cart([]);
 // (the things in the Product.allProducts array) into the drop down list.
 function populateForm() {
 
-  //TODO: Add an <option> tag inside the form's select for each product
+  //TODO: Add an <option> tag inside the form's select for each product [done]
   var selectElement = document.getElementById('items');
   for (var i in Product.allProducts) {
-
+      var option = document.createElement('option');
+      option.setAttribute('value',Product.allProducts[i].name);
+      option.textContent = Product.allProducts[i].name;
+      selectElement.appendChild(option);
   }
-
 }
 
 // When someone submits the form, we need to add the selected item to the cart
@@ -22,7 +24,8 @@ function populateForm() {
 // so that it shows the # of items in the cart and a quick preview of the cart itself.
 function handleSubmit(event) {
 
-  // TODO: Prevent the page from reloading
+  // TODO: Prevent the page from reloading [done]
+  event.preventDefault();
 
   // Do all the things ...
   addSelectedItemToCart();
@@ -37,10 +40,23 @@ function addSelectedItemToCart() {
   // TODO: suss out the item picked from the select list
   // TODO: get the quantity
   // TODO: using those, add one item to the Cart
+  var getItems = document.getElementById('items');
+  var selectedItem = getItems.options[getItems.selectedIndex].text;
+  var getQuantity = document.getElementById('quantity');
+  var quantity = getQuantity.value;
+  cart.addItem(selectedItem,quantity);
 }
 
 // TODO: Update the cart count in the header nav with the number of items in the Cart
-function updateCounter() {}
+function updateCounter() {
+  var span = document.getElementById('itemCount');
+  var total = 0;
+  for(var i in cart.items) {
+    var num = Number(cart.items[i].quantity);
+    total = total + num;
+  }
+  span.textContent = " " + total;
+}
 
 // TODO: As you add items into the cart, show them (item & quantity) in the cart preview div
 function updateCartPreview() {
